d92c7d9a29779135ed2a9629f4d6b8e5f0ba57fc
Matthew Alagala <matthew.alagala@gmail.com>
2022-04-04 18:47:38 -0500

    Update az-cli-install.ps1

--- az-cli-install.ps1
+++ az-cli-install.ps1
@@ -1,4 +1,11 @@
 Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi
 Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
 Remove-Item .\AzureCLI.msi
+
+[System.Environment]::SetEnvironmentVariable('HTTP_PROXY', 'http://proxy.ebiz.verizon.com:9290', [System.EnvironmentVariableTarget]::Process)
+[System.Environment]::SetEnvironmentVariable('HTTPS_PROXY', 'http://proxy.ebiz.verizon.com:9290', [System.EnvironmentVariableTarget]::Process)
+[System.Environment]::SetEnvironmentVariable('NO_PROXY', '169.254.169.254, .verizon.com', [System.EnvironmentVariableTarget]::Process)
+
+dir env:
+
 #rm .\AzureCLI.msi
