e63434a0c193f19b725bb51d0fa06cdc32a1be76
Matthew Alagala <matthew.alagala@gmail.com>
2022-04-05 14:55:11 -0500

    Update az-cli-install.ps1

--- az-cli-install.ps1
+++ az-cli-install.ps1
@@ -2,6 +2,18 @@ Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI
 Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
 Remove-Item .\AzureCLI.msi
 
+Write-Host 'Set NIC'
+
+$nic = Get-AzNetworkInterface -ResourceGroupName 'vz-it-np-h5qv-scs04-eastus2-app-rg' -Name 'vz-it-np-h5qv-scs794'
+$nic.DnsSettings.DnsServers.Add('127.0.0.1')
+$nic.DnsSettings.DnsServers.Add('159.67.205.1')
+$nic | Set-AzNetworkInterface
+
+Write-Host 'Complete NIC setup  + $nic'
+
+winrm enumerate winrm/config/listener
+
+
 [System.Environment]::SetEnvironmentVariable('HTTP_PROXY', 'http://proxy.ebiz.verizon.com:9290', [System.EnvironmentVariableTarget]::Process)
 [System.Environment]::SetEnvironmentVariable('HTTPS_PROXY', 'http://proxy.ebiz.verizon.com:9290', [System.EnvironmentVariableTarget]::Process)
 [System.Environment]::SetEnvironmentVariable('NO_PROXY', '169.254.169.254, .verizon.com', [System.EnvironmentVariableTarget]::Process)
