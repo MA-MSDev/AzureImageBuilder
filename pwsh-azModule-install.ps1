2615a2ed5ce8bae96c8a1864042127e11a934324
Matthew Alagala <matthew.alagala@gmail.com>
2022-04-06 17:12:04 -0500

    Update pwsh-azModule-install.ps1

--- pwsh-azModule-install.ps1
+++ pwsh-azModule-install.ps1
@@ -1,3 +1,5 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
 # Install PowerShell Az Module
 
 Install-Module -Name Az -Repository PSGallery -AllowClobber -AllowPrerelease -Force -Confirm:$false
