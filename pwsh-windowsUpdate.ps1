792194a2a9fd1cde81cb5be54cd56dae7f03d55f
Matthew Alagala <matthew.alagala@gmail.com>
2022-04-07 14:45:00 -0500

    Update pwsh-windowsUpdate.ps1

--- pwsh-windowsUpdate.ps1
+++ pwsh-windowsUpdate.ps1
@@ -1,7 +1,9 @@
 # Confirm Az Module install
 
-Get-InstalledModule
+pwsh -Command {Get-InstalledModule
 
 Write-Host 'Confirm Az Module Installed'
 
 Install-Module -Name PSWindowsUpdate -Repository PSGallery -Force -AllowClobber
+
+Get-InstalledModule}
