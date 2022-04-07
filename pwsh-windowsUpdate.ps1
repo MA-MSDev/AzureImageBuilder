1e0902a0688cf997a62c5ab0a0c69fcd91eaf6db
Matthew Alagala <matthew.alagala@gmail.com>
2022-04-06 22:35:16 -0500

    Update pwsh-windowsUpdate.ps1

--- pwsh-windowsUpdate.ps1
+++ pwsh-windowsUpdate.ps1
@@ -1,1 +1,7 @@
-Install-Module PSWindowsUpdate -Force
+# Confirm Az Module install
+
+Get-InstalledModule
+
+Write-Host 'Confirm Az Module Installed'
+
+Install-Module -Name PSWindowsUpdate -Repository PSGallery -Force -AllowClobber
