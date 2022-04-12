2d5f2d626f1fbf72be4e992edf37571cb1947014
Matthew Alagala <matthew.alagala@gmail.com>
2022-04-12 11:40:01 -0500

    Update pwsh-get.ps1

--- pwsh-get.ps1
+++ pwsh-get.ps1
@@ -1,6 +1,6 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-Install-Module -Name NuGet -MinimumVersion 2.8.5.201 -Force
+# Install-Module -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
 Install-Module -Name PackageManagement -Repository PSGallery -Force -AllowClobber
 
