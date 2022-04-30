b7f21f27f3ace5f2e27c6ca45891f6c78f27a5d3
Matthew Alagala <matthew.alagala@gmail.com>
2022-04-29 19:45:14 -0500

    updated ps install

--- pwsh-install.ps1
+++ pwsh-install.ps1
@@ -1,10 +1,3 @@
-# Set env variables
-[System.Environment]::SetEnvironmentVariable('HTTP_PROXY', 'http://proxy.ebiz.verizon.com:9290', [System.EnvironmentVariableTarget]::Process)
-[System.Environment]::SetEnvironmentVariable('HTTPS_PROXY', 'http://proxy.ebiz.verizon.com:9290', [System.EnvironmentVariableTarget]::Process)
-[System.Environment]::SetEnvironmentVariable('NO_PROXY', '.verizon.com', [System.EnvironmentVariableTarget]::Process)
-
 Set-ExecutionPolicy Bypass -Scope Process -Force
-
-dir env:
 
 Invoke-Expression "& {$(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1')} -Quiet -UseMSI"
